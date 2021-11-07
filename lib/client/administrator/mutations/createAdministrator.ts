b88deb971f15ebc5768a1b400900ba28fdcb2515
femyeda <femi@individual.company>
2021-11-07 09:41:43 -0600

    remove administrator schema crud, client

--- lib/client/administrator/mutations/createAdministrator.ts
+++
@@ -1,9 +0,0 @@
-import { Prisma } from "@db/index"
-import superagent from "superagent"
-import { ADMINISTRATOR_API_URL } from ".."
-
-export const createAdministrator = async (args: Prisma.AdministratorCreateArgs) => {
-  const response = await superagent.post(`${ADMINISTRATOR_API_URL}/create`).send({ data: args })
-
-  return response.body.data
-}
