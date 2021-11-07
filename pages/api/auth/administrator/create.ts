58d64601e80a8f22f13a472e12e656d015252e53
femyeda <femi@individual.company>
2021-11-07 12:03:48 -0600

    configure admin login with role based auth

--- pages/api/auth/administrator/create.ts
+++ pages/api/auth/administrator/create.ts
@@ -1,8 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import nc from "next-connect";
 import { createHash } from "crypto";
-import { _createAdministrator } from "@api/administrator/_operations";
 import { hashPassword } from "@lib/auth/passwords";
+import { _createUser } from "@api/user/_operations";
 
 /**
  * https://github.com/nextauthjs/next-auth/blob/main/src/server/lib/cookie.js
@@ -36,18 +36,18 @@ const validateCSRFToken = (req: NextApiRequest, secret: string): boolean => {
 const post = async (req: NextApiRequest, res: NextApiResponse) => {
   const secret = process.env.NEXTAUTH_SECRET;
 
-  // TODO: NOT WORKING FOR SOME VERY FUCKING STRANGE REASON
-  // if (!validateCSRFToken(req, secret)) {
-  //   return res.status(401).json({
-  //     message: "NOPE",
-  //   });
-  // }
+  if (!validateCSRFToken(req, secret)) {
+    return res.status(401).json({
+      message: "NOPE",
+    });
+  }
   try {
-    const admin = await _createAdministrator({
+    const admin = await _createUser({
       data: {
         name: req.body.name,
         email: req.body.email,
         password: await hashPassword(req.body.password),
+        role: "admin",
       },
       select: {
         id: true,
@@ -61,7 +61,7 @@ const post = async (req: NextApiRequest, res: NextApiResponse) => {
       data: admin,
     });
   } catch (error) {
-    console.error("[api] user/create", error);
+    console.error("[api] auth/administrator/create", error);
     return res.status(500).json({ statusCode: 500, message: error.message });
   }
 };
