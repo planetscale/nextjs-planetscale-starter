db8a02bb8f0b4e3a9a56e4f103b893a3a33c1078
Taylor Barnett <taylor@planetscale.com>
2021-11-10 22:18:54 -0700

    update index page with repo link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,21 @@
 import AppLayout from "@lib/components/Layouts/AppLayout";
+import Image from 'next/image';
 
 const Page = () => {
   return (
     <>
       <AppLayout>
+        {/* <blockquote> */}
+        <h1>Welcome to the PlanetScale Next.js Starter App!</h1>
+        <p>
+          This is an example site to demonstrate how to use{" "}
+          <a href={`https://next-auth.js.org`}>NextAuth.js</a> for
+          authentication with PlanetScale and Prisma.
+        </p>
         <blockquote>
-          <h1>Welcome to the PlanetScale Next.js Starter App!</h1>
-
-          <p>
-            This is an example site to demonstrate how to use{" "}
-            <a href={`https://next-auth.js.org`}>NextAuth.js</a> for
-            authentication.
+          <p>  
+            You can find how to get started{" "}
+            <a href={`https://github.com/planetscale/nextjs-planetscale-starter`}>here</a>.
           </p>
         </blockquote>
       </AppLayout>
